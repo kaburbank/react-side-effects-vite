15fe957cf3da52c4cc9ae8ad5f0ef7a3006c9375
Katherine <kaburbank@gmail.com>
2026-01-05 09:57:56 -0700

    lab completed

--- src/components/FetchButton.jsx
+++ src/components/FetchButton.jsx
@@ -5,6 +5,6 @@ const FetchButton = ({ onFetchJoke }) => {
     // Step 2: Add an onClick event that calls `fetchJoke`
     <button className="fetch-button" onClick={onFetchJoke}>Get a New Joke</button>
   );
-}
+};
 
 export default FetchButton;
